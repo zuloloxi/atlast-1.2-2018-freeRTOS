756c877b06a45828f8613bfa52484c8cb3ccdd7b
andrewtholt <andrewtholt60@gmail.com>
2016-11-19 15:37:27 +0000

    Small changes

--- atlast-1.2/srcToArray.sh
+++ atlast-1.2/srcToArray.sh
@@ -6,7 +6,9 @@ printf "uint8_t nvramrc[] = \""
 
 while read -r LINE; do 
     if [ ${#LINE} -ge 0 ]; then
-        printf "$LINE \\n"| tr '\n' ' ' | sed 's/  */ /g'
+#        printf "$LINE \\n"| tr '\n' ' ' | sed 's/  */ /g'
+        printf "$LINE \\" | sed '/^\s*$/d'
+        printf "n"
     fi
 done 
 printf "\";\n"
