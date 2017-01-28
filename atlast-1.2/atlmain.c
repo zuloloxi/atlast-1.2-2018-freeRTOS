4f4e8d5448d74a2655d3f2ca4abc137177bc110a
andrewtholt <andrewtholt>
2017-01-28 16:56:36 +0000

    Commented out global callbackuntil I find the best way to do this.

--- atlast-1.2/atlmain.c
+++ atlast-1.2/atlmain.c
@@ -101,7 +101,7 @@ void *doSmall(void *arg) {
     bool ff;
     struct Small *myTable=arg;
 
-    ff=setGlobalCallback(myTable, doSmallCallback);
+//    ff=setGlobalCallback(myTable, doSmallCallback);
 
     // This lock is held by this threads parent.
     // Once the parent has completed its setup it will release the lock, and
